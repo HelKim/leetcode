d51e0bce2bb7ad287290ae380f3d9676a8ade75b
helkim <zhengxiaojian@outlook.com>
2020-11-20 14:47:10 +0800

    add cpp 235 236

---
+++ cpp/problem/236.h
@@ -0,0 +1,27 @@
+//
+// Created by zhengxiaojian on 2020/11/20.
+//
+
+#ifndef CPP_236_H
+#define CPP_236_H
+
+#include "../lib/TreeNode.h"
+
+using namespace std;
+
+class Solution {
+public:
+    TreeNode *lowestCommonAncestor(TreeNode *root, TreeNode *p, TreeNode *q) {
+        if (root == nullptr) return nullptr;
+
+        if (root->val == p->val || root->val == q->val) return root;
+
+        auto *left = lowestCommonAncestor(root->left, p, q);
+        auto *right = lowestCommonAncestor(root->right, p, q);
+
+        if (left && right) return root;
+        return left == nullptr ? right : left;
+    }
+};
+
+#endif //CPP_236_H
