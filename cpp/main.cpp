8a8133a63d8cd946964e18796865e1ec29b35f9f
helkim <zhengxiaojian@outlook.com>
2020-11-29 16:28:14 +0800

    add cpp 10

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,12 +1,26 @@
 
 #include <iostream>
 #include "problem/backtrack/10.h"
+#include <unordered_map>
 
 using namespace std;
 
 int main() {
-
-    Solution s;
-    cout << s.isMatch("bccbbabcaccacbcacaa", ".*b.*c*.*.*.c*a*.c");
+    int a = 12;
+    int *p = &a;
     return 0;
+}
+
+struct Scope {
+    std::unordered_map<std::string, void *> variables;
+    Scope *parent;
+};
+
+void *get_val(const Scope *scope, const std::string &name) {
+    if (scope == nullptr) return nullptr;
+    auto p = scope->variables.find(name);
+    if (p != scope->variables.end()) {
+        return p->second;
+    }
+    return get_val(scope->parent, name);
 }
